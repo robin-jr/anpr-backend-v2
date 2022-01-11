912161d68c9ea5922d6628b1101a3caa65d99e32
Raveen2001 <raveenanbarasan@gmail.com>
2022-01-11 22:41:16 +0530

    debug api for file open added

--- anpr/views.py
+++ anpr/views.py
@@ -850,27 +850,40 @@ def exportToUsbv2(request):
 
 
 def debug(request):
-    platesQuerySet=LicensePlates.objects.filter(plate_number__contains = "TN")
+
+    imagePath = imageLocation + "noImage.jpg"
+
+    try:
+        with Image.open(imagePath) as img:
+            print("Success")        
+    except Exception as e:
+        print("Failed")
+
+            
+
+
+
+    # platesQuerySet=LicensePlates.objects.filter(plate_number__contains = "TN")
 
     # entriesQuerySet = LicensePlates.objects.filter.select_related('vehicle_type').select_related('vehicle_make').select_related('vehicle_model').select_related('vehicle_color').order_by('-entry_id')
     # recognitionCount = LicensePlates.objects.count()
-    plateQuerySet = LicensePlates.objects.select_related('vehicle_type').select_related('vehicle_make').select_related('vehicle_model').select_related('vehicle_color').order_by('-entry_id')
-    entries = []
-    for data in plateQuerySet:
-        temp={}
-        temp["id"]= data.pk
-        temp["camera_name"]= data.camera_name
-        # temp["junction_name"]= e.junction_name
-        temp["plate"]= data.plate_number
-        temp["date"]= data.date.strftime('%d/%m/%Y %H:%M:%S')
-        temp["anpr_full_image"]= data.anpr_full_image
-        temp["anpr_cropped_image"]= data.anpr_cropped_image
-        temp["vehicle_type"] = data.vehicle_type.name
-        temp["vehicle_make"] = data.vehicle_make.name
-        temp["vehicle_model"] = data.vehicle_model.name
-        temp["vehicle_color_name"] = data.vehicle_color.name
-        temp["vehicle_color_code"] = data.vehicle_color.code
-        entries.append(temp)
+    # plateQuerySet = LicensePlates.objects.select_related('vehicle_type').select_related('vehicle_make').select_related('vehicle_model').select_related('vehicle_color').order_by('-entry_id')
+    # entries = []
+    # for data in plateQuerySet:
+    #     temp={}
+    #     temp["id"]= data.pk
+    #     temp["camera_name"]= data.camera_name
+    #     # temp["junction_name"]= e.junction_name
+    #     temp["plate"]= data.plate_number
+    #     temp["date"]= data.date.strftime('%d/%m/%Y %H:%M:%S')
+    #     temp["anpr_full_image"]= data.anpr_full_image
+    #     temp["anpr_cropped_image"]= data.anpr_cropped_image
+    #     temp["vehicle_type"] = data.vehicle_type.name
+    #     temp["vehicle_make"] = data.vehicle_make.name
+    #     temp["vehicle_model"] = data.vehicle_model.name
+    #     temp["vehicle_color_name"] = data.vehicle_color.name
+    #     temp["vehicle_color_code"] = data.vehicle_color.code
+    #     entries.append(temp)
     
         # pass 
         # print(data.camera_name)
